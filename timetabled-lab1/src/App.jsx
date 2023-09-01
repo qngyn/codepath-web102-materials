7d1a17eeeef12481afd2ef3dbf2dcce08a228a31
Quynh Nguyen <qtn2@case.edu>
2023-09-01 13:53:56 -0400

    finish the first lab

---
+++ timetabled-lab1/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css';
+import Calendar from './components/Calendar'
+
+const App = () => {
+
+  return (
+    <div className="App">
+      <h1> Itinerary for 7 Days in Chicago &#x1F3D9; </h1>
+      <h2> One week in Chicago is wonderful. If you want to get to know the city and see all the sights, then spending 7 days in Chicago is perfect</h2>
+      <Calendar />
+    </div>
+  )
+}
+
+export default App
+
